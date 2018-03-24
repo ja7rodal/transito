f065339d6592afe500ac4c5b8ccfd82208892ad6
Jasson Rodirguez <jrodriguez@portalfinance.co>
2018-03-23 21:08:00 -0500

    validations and radio-button

--- app/models/person.rb
+++ app/models/person.rb
@@ -15,8 +15,10 @@
 class Person < ApplicationRecord
 
 # Validations
-  validates :name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :last_name, presence: true, length: {maximum: 20 }
+  validates :email, presence: true, uniqueness: true
+  validates :description, length: { minimum: 20 }
+  validates :age, numericality: { only_integer: true, greater_than: 18, less_than: 90 } 
 
 end
